7738f46e2d1044b849af50e775b038eccd5585ef
mk <michaelkim291@gmail.com>
2024-11-03 22:22:50 -0500

    added basic comments and left/right movement of camera

--- src/UI/Camera/Camera.cpp
+++ src/UI/Camera/Camera.cpp
@@ -7,14 +7,22 @@
 #include <glm/gtc/matrix_transform.hpp>
 //#include <glm/gtx/rotate_vector.hpp>
 
-
+//Default values of where camera should be in world
+//TEMPORARY VALUES - NEEDS TO CHANGE ONCE WE STOP USING 0.obj AS TESTING
 Camera::Camera(){
+    //location of camera in world
     mEye = glm::vec3(20.0f, 100.0f, 200.0f);
-    mViewDirection = glm::vec3(0.0f, 110.0f, -1.0f);
+
+    //Direction which camera is facing
+    //must subtract mEye for default value because mEye gets added to mViewDirection
+    mViewDirection = glm::vec3(0.0f, 110.0f, -1.0f) - mEye;
+    
+    //For finding the rightside up
     mUpVector = glm::vec3(0.0f, 1.0f, 0.0f);
+
+    //Holding previous frame's mouse position
     mOldMousePosition = glm::vec2(0.0f, 0.0f);
 
-    mViewDirection -= mEye; 
 }
 
 glm::mat4 Camera::GetViewMatrix() const{
@@ -23,12 +31,15 @@ glm::mat4 Camera::GetViewMatrix() const{
 }
 
 
-// Convert mViewDirection to a 4D vector, apply rotation, and convert back to 3D
+
 void Camera::MouseLook(int mouseX, int mouseY){
     //float mX = static_cast<float>(mouseX);
     //float mY = static_cast<float>(mouseY);
-    glm::vec2 CurrentMousePosition(mouseX, mouseY); 
+    float mX_Sensitivity = 0.05f;
+    float mY_Sensitivity = 0.03f;
+    glm::vec2 CurrentMousePosition(mouseX*mX_Sensitivity, mouseY*mY_Sensitivity); 
 
+    //makes sure that mOldMousePosition does not start null, and instead has a beginning
     static bool firstLook = true;
     if (firstLook) {
         mOldMousePosition = CurrentMousePosition;
@@ -36,23 +47,34 @@ void Camera::MouseLook(int mouseX, int mouseY){
     }
     glm::vec2 mouseDelta = mOldMousePosition - CurrentMousePosition; 
 
-    glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(mouseDelta.x), mUpVector);
-    mViewDirection = glm::vec3(rotationMatrix * glm::vec4(mViewDirection, 0.0f));
+    // Convert mViewDirection to a 4D vector, apply rotation, and convert back to 3D
+    // Rotation around the y-axis (yaw) - horizontal movement
+    glm::mat4 yawRotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(mouseDelta.x), mUpVector);
+    mViewDirection = glm::vec3(yawRotationMatrix * glm::vec4(mViewDirection, 0.0f));
+
+    // Rotation around the x-axis (pitch) - vertical movement
+    glm::vec3 rightVector = glm::normalize(glm::cross(mViewDirection, mUpVector));
+    glm::mat4 pitchRotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(-mouseDelta.y), -rightVector);
+    mViewDirection = glm::vec3(pitchRotationMatrix * glm::vec4(mViewDirection, 0.0f));
+
     mOldMousePosition = CurrentMousePosition;
-    //std::cout << mViewDirection.x << ", " << mViewDirection.y << ", " << mViewDirection.z << std::endl;
-    std::cout << mouseX << std::endl;
+    std::cout << mViewDirection.x << ", " << mViewDirection.y << ", " << mViewDirection.z << std::endl;
+    //std::cout << mouseX << std::endl;
 }
 
+
+//Names indicate which direction the camera moves
 void Camera::MoveForward(float speed) {
-    //std::cout << "Forward" << std::endl;
     mEye += (mViewDirection * speed);
 }
 void Camera::MoveBack(float speed) {
     mEye -= (mViewDirection * speed);
 }
 void Camera::MoveLeft(float speed) {
-    //mEye.x -= speed;
+    glm::vec3 rightVector = glm::cross(mViewDirection, mUpVector);
+    mEye -= (rightVector * speed);
 }
 void Camera::MoveRight(float speed) {
-    //mEye.x += speed;
+    glm::vec3 rightVector = glm::cross(mViewDirection, mUpVector);
+    mEye += (rightVector * speed);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
-#include <stdio.h> /* printf and fprintf */
-#include <iostream>
-#include <fstream>
-#include <string>
-
 #include <Headers/Window.hpp>
 #include <Headers/ObjLoader.hpp>
 #include <Headers/Renderer.hpp>
 #include <Headers/Shaders.hpp>
 #include <Headers/UserInput.hpp>
 #include <Headers/Camera.hpp>
+
+#include <stdio.h> /* printf and fprintf */
+#include <iostream>
+#include <fstream>
+#include <string>
 
 //Window and graphics stuff
 #include <SDL2/SDL.h>
@@ -17,12 +17,21 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+
+//ABSTRACT MORE OF THIS CODE IN THE FUTURE!!!
+
+//SDL window where all the magic happens
 Window main_window;
+
+//determines if program continues or quits
 bool gQuit = false;
+
+//for holding shaders
 GLint core_program;
+
+//for holding all shapes being loaded
 std::vector<ObjLoader> shape_dictionary;
 
-//TEMPORARY
 /*
 glm::mat4 model;
 glm::mat4 view;
@@ -30,7 +39,7 @@ glm::mat4 projection;
 glm::mat4 mvp;
 */
 
-float g_uOffset = 0.0f;
+//Camera Object for moving cameran in first person
 Camera gCamera;
 
 
@@ -39,30 +48,42 @@ void InitializeProgram() {
     main_window.CreateWindow(1440, 1440);
 }   
 
+//load the OBJ file, then generating the vertex buffers
 void VertexSpecification() {
+    //Load OBJ file
     std::string obj_path = "temp_asset_folder/data/faces/0.obj";
     ObjLoader test_obj(obj_path);
-    Renderer temp_rend; //for testing
-
     shape_dictionary.push_back(test_obj);
+
+    //generate vertex buffers
+    //Most likely a function will be created elsewhere, and called in main loop in order to add more shapes down the line!
+    Renderer temp_rend; //for testing
     temp_rend.Generate_VAO_VBO_EBO(shape_dictionary[0]);
 }
 
+//Graphics pipeline. So far, just loading shaders
 void CreateGraphicsPipeline() {
     //load shaders
     Shaders gl_shader(core_program);
 }
 
+//Gets user input, and handles them
 void Input() {
+    //for getting every single input user can make in SDL
     SDL_Event event;
+
+    //defines default values for x and y mouse movement
     static int mouseX = main_window.get_gScreenWidth()/2; 
     static int mouseY = main_window.get_gScreenHeight()/2;
     
+    //Goes through every single input, and handles them before exiting
     while( SDL_PollEvent(&event) != 0 ) {
+        //if the input is quit, we quit the whole program
         if(event.type == SDL_QUIT) {
             gQuit = true;
             break;
         }
+        //if the mouse is moved at all, RELATIVE x and y values are passed into the camera MouseLook function
         if(event.type == SDL_MOUSEMOTION) {
             mouseX += event.motion.xrel;
             mouseY += event.motion.yrel;
@@ -70,6 +91,8 @@ void Input() {
         }
     }
 
+    //Determines if user pressed WASD, and moves camera accordingly
+    //Does so by getting a list of all keys, and checking if they have been pressed
     const Uint8 *state = SDL_GetKeyboardState(NULL);
     float speed = 0.1f;
     if(state[SDL_SCANCODE_W]) {
@@ -86,6 +109,7 @@ void Input() {
     }
 }
 
+//we load up the rendering parameters, the camera, and the shaders here
 void PreDraw() {
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
@@ -99,7 +123,7 @@ void PreDraw() {
     glm::mat4 model = glm::mat4(1.0f);
     glm::mat4 view = gCamera.GetViewMatrix();
     //glm::mat4 projection = glm::perspective(glm::radians(30.0f), 1.0f, 0.1f, 1000.0f);
-    glm::mat4 projection = glm::perspective(glm::radians(30.0f), 1.0f, 0.1f, 1000.0f);
+    glm::mat4 projection = glm::perspective(glm::radians(30.0f), 1.0f, 0.1f, 10000.0f);
     glm::mat4 mvp = projection * view;
 
     // Draw shaders
@@ -111,15 +135,15 @@ void PreDraw() {
 //here's where the actual models gets rendered
 void Draw() {
     glBindVertexArray(shape_dictionary[0].VAO);
-    //glm::mat4 model = glm::mat4(1.0f);
-    /*
+    glm::mat4 model = glm::mat4(1.0f);
     model = glm::mat4(1.0f);
     model = glm::rotate(model, glm::radians(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
-    */
     glDrawArrays(GL_TRIANGLES, 0, shape_dictionary[0].shapes[0].mesh.indices.size());
 }
 
+//We trap the mouse, and then loop the game logic
+//Input, PreDraw, Draw, and swap_window for updating SDL logics 
 void MainLoop() {
     SDL_WarpMouseInWindow(main_window.gGraphicsApplicationWindow, main_window.get_gScreenWidth()/2, main_window.get_gScreenHeight()/2);
     SDL_SetRelativeMouseMode(SDL_TRUE);
@@ -132,12 +156,20 @@ void MainLoop() {
     }
 }
 
+//deletes windows, VAO, VBO, and EBO from &shape_diectory
 void CleanUp() {
     main_window.Window_CleanUp();
+    for(int i = 0; i < shape_dictionary.size(); i++ ) {
+        glDeleteVertexArrays(1, &shape_dictionary[0].VAO);
+        glDeleteBuffers(1, &shape_dictionary[0].VBO);
+        glDeleteBuffers(1, &shape_dictionary[0].EBO);
+    }
+    glDeleteProgram(core_program);
 }
 
 
 
+//This is where everything begins.
 int main () {
     InitializeProgram();
     VertexSpecification();
